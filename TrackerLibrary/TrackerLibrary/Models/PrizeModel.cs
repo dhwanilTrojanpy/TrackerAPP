0735260136602219d87ef0942bea3374e0456040
Harshil Bhavsar <harshilb150799@gmail.com>
2024-06-19 10:50:41 -0400

    Moved all model files to the Model Filder.

---
+++ TrackerLibrary/TrackerLibrary/Models/PrizeModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class PrizeModel
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Represents the place number of the team.
+        /// </summary>
+        public int PlaceNumber { get; set; }
+        /// <summary>
+        /// Represents the place name of the team.
+        /// </summary>
+        public string PlaceName { get; set; }
+        /// <summary>
+        /// Represents the prize amount according to the place.
+        /// </summary>
+        public decimal PrizeAmount { get; set; }
+        /// <summary>
+        /// Represents the prize percentage.
+        /// </summary>
+        public double Percentage { get; set; }
+
+        public PrizeModel()
+        {
+
+        }
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+
+            int placeNumberValue = 0;
+            int.TryParse(placeNumber, out placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            Percentage = prizePercentageValue;
+
+        }
+
+    }
+}
